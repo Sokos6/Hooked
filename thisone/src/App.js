c63f1ab5f8b512dcc53ea2d0fe93c105f76cd047
Sokos6 <will.sokolowski@gmail.com>
2019-12-27 01:55:28 -0600

    userbar

--- thisone/src/App.js
+++ thisone/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 // import Login from './user/Login';
 // import Logout from './user/Logout';
-import Register from './user/Register';
+// import Register from './user/Register';
+import UserBar from './user/UserBar';
 
 export default function App() {
-  return <Register />
+  return <UserBar />
 
   // <Logout user="Will Sokolowski" />
 
