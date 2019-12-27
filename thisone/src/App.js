8cf1699c8e86084eb13d26fd60b428dacaad244e
Sokos6 <will.sokolowski@gmail.com>
2019-12-27 02:06:10 -0600

    post

--- thisone/src/App.js
+++ thisone/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 // import Login from './user/Login';
 // import Logout from './user/Logout';
 // import Register from './user/Register';
-import UserBar from './user/UserBar';
+// import UserBar from './user/UserBar';
+import Post from './post/Post';
 
 export default function App() {
-  return <UserBar />
+
+  return <Post title="Hooked" content="Stuck" author="Will Sokolowski" />
+
+  // return <UserBar />
 
   // <Logout user="Will Sokolowski" />
 
