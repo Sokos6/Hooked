a610d52168b527d72b6e757dab82b75c511cf8e1
Sokos6 <will.sokolowski@gmail.com>
2019-12-27 01:32:37 -0600

    register

--- thisone/src/App.js
+++ thisone/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 // import Login from './user/Login';
-import Logout from './user/Logout';
+// import Logout from './user/Logout';
+import Register from './user/Register';
 
 export default function App() {
-  return <Logout user="Will Sokolowski" />
+  return <Register />
+
+  // <Logout user="Will Sokolowski" />
 
   // <Login />
 }
