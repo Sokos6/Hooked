9dd1f33acf8e1d390f6b9f984cd6275c68146e5f
Sokos6 <will.sokolowski@gmail.com>
2019-12-27 02:19:30 -0600

    post list

--- thisone/src/App.js
+++ thisone/src/App.js
@@ -1,19 +1,12 @@
-import React from 'react';
-// import Login from './user/Login';
-// import Logout from './user/Logout';
-// import Register from './user/Register';
-// import UserBar from './user/UserBar';
-// import Post from './post/Post';
-import CreatePost from './post/CreatePost';
+import React from 'react'
 
-export default function App() {
+import PostList from './post/PostList'
 
-    return <CreatePost />
-  // return <Post title="Hooked" content="Stuck" author="Will Sokolowski" />
+const posts = [
+    { title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Will Sokolowski' },
+    { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Will Sokolowski' }
+]
 
-  // return <UserBar />
-
-  // <Logout user="Will Sokolowski" />
-
-  // <Login />
+export default function App () {
+    return <PostList posts={posts} />
 }
