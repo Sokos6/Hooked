69c2f2abb73520aa32c53f91f275430544f414ad
Sokos6 <will.sokolowski@gmail.com>
2019-12-29 20:05:39 -0600

    working ui

--- thisone/src/App.js
+++ thisone/src/App.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-import PostList from './post/PostList'
+import PostList from './post/PostList';
+import CreatePost from './post/CreatePost';
+import UserBar from './user/UserBar';
 
+const user = 'Will Sokolowski'
 const posts = [
-    { title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Will Sokolowski' },
-    { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Will Sokolowski' }
+  { title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Daniel Bugl' },
+  { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Daniel Bugl' }
 ]
 
-export default function App () {
-    return <PostList posts={posts} />
+export default function App() {
+  return (
+    <div style={{ padding: 8 }}>
+      <UserBar />
+      <br />
+      <CreatePost user={user} />
+      <br />
+      <hr />
+      <PostList posts={posts} />
+    </div>
+  )
 }
