39ad46b5adb922f4f1528539ae8dcaac1e3b7baa
Sokos6 <will.sokolowski@gmail.com>
2020-01-05 17:49:04 -0600

    working themes

--- thisone/src/App.js
+++ thisone/src/App.js
@@ -1,9 +1,11 @@
-import React, { useReducer, useEffect } from "react";
+import React, { useReducer, useEffect, useState } from "react";
 import PostList from "./post/PostList";
 import CreatePost from "./post/CreatePost";
 import UserBar from "./user/UserBar";
-import appReducer from './reducers';
-import Header from './Header';
+import appReducer from "./reducers";
+import Header from "./Header";
+import { ThemeContext } from "./contexts";
+import ChangeTheme from "./ChangeTheme";
 
 const defaultPosts = [
   {
@@ -18,32 +20,44 @@ const defaultPosts = [
   }
 ];
 
-export const ThemeContext = React.createContext({ primaryColor: 'deepskyblue' });
+// export const ThemeContext = React.createContext({ primaryColor: 'deepskyblue' });
 
 export default function App() {
-  const [ state, dispatch ] = useReducer(appReducer, { user: '', posts: defaultPosts });
+  const [theme, setTheme] = useState({
+    primaryColor: "deepskyblue",
+    secondaryColor: "coral"
+  });
+
+  const [state, dispatch] = useReducer(appReducer, {
+    user: "",
+    posts: defaultPosts
+  });
   const { user, posts } = state;
 
   useEffect(() => {
     if (user) {
-        document.title = `${user} - React Hooks Blog`
+      document.title = `${user} - React Hooks Blog`;
     } else {
-        document.title = 'React Hooks Blog'
+      document.title = "React Hooks Blog";
     }
-}, [user])
+  }, [user]);
 
   return (
-    <div style={{ padding: 8 }}>
-    <Header text="Go Go React" />
-    <div style={{ padding: 8 }}>
-      <Header text="Hooked" />
-      <UserBar user={user} dispatch={dispatch} />
-      <br />
-      {user && <CreatePost user={user} posts={posts} dispatch={dispatch} />}
-      <br />
-      <hr />
-      <PostList posts={posts} />
-    </div>
-    </div>
+    <ThemeContext.Provider value={theme}>
+      <div style={{ padding: 8 }}>
+        <Header text="Go Go React" />
+        <ChangeTheme theme={theme} setTheme={setTheme} />
+        <br />
+        <div style={{ padding: 8 }}>
+          <Header text="Hooked" />
+          <UserBar user={user} dispatch={dispatch} />
+          <br />
+          {user && <CreatePost user={user} posts={posts} dispatch={dispatch} />}
+          <br />
+          <hr />
+          <PostList posts={posts} />
+        </div>
+      </div>
+    </ThemeContext.Provider>
   );
 }
