8333641f8cff6615bcd79176b34e531519fe412e
Sokos6 <will.sokolowski@gmail.com>
2020-01-19 01:49:00 -0600

    routing

--- thisone/src/App.js
+++ thisone/src/App.js
@@ -1,20 +1,30 @@
-import React, { useReducer, useEffect, useState } from 'react'
+import React, { useReducer, useEffect, useState } from 'react';
+import { Router, View } from 'react-navi';
+import { mount, route } from 'navi';
 
-import PostList from './post/PostList'
-import CreatePost from './post/CreatePost'
-import UserBar from './user/UserBar'
-import Header from './Header'
+// import PostList from './post/PostList'
+// import CreatePost from './post/CreatePost'
+// import UserBar from './user/UserBar'
+// import Header from './Header'
 import { ThemeContext, StateContext } from './contexts'
-import ChangeTheme from './ChangeTheme'
+// import ChangeTheme from './ChangeTheme'
 import appReducer from './reducers'
-import { useResource } from 'react-request-hook';
+// import { useResource } from 'react-request-hook';
 import HeaderBar from './pages/HeaderBar';
 import HomePage from './pages/HomePage';
+import PostPage from './pages/PostPage';
 
 export default function App () {
     const [ theme, setTheme ] = useState({
         primaryColor: 'deepskyblue',
         secondaryColor: 'coral'
+    })
+
+    const routes = mount({
+      '/': route({ view: <HomePage /> }),
+      '/view/:id': route(req => {
+        return { view: <PostPage id={req.params.id} /> }
+      }),
     })
 
     const [ state, dispatch ] = useReducer(appReducer, { user: '', posts: [], error: '' })
@@ -32,11 +42,14 @@ export default function App () {
     return (
         <StateContext.Provider value={{ state, dispatch }}>
             <ThemeContext.Provider value={theme}>
+                <Router routes={routes}>
                 <div style={{ padding: 8 }}>
                   <HeaderBar setTheme={setTheme} />
                     <hr />
-                    <HomePage />
+                    {/* <PostPage id={'react-hooks'} /> */}
+                    <View />
                 </div>
+                </Router>
             </ThemeContext.Provider>
         </StateContext.Provider>
     )
