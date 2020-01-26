def90d3acb1040add1d1e9537671561c7a0b89ab
Sokos6 <will.sokolowski@gmail.com>
2020-01-25 23:57:44 -0600

    working state

---
+++ stateside/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import LessText from './LessText';
+
+function App() {
+  return (
+    <div className="App">
+      <LessText text={`Focused, hard work is the real key
+      to success. Keep your eyes on the goal,
+      and just keep taking the next step
+      towards completing it.`}
+    maxLength={35}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ stateside/src/LessText.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+function LessText({ text, maxLength }) {
+
+  const [hidden, setHidden ] = useState(true);
+
+  if(text.length <= maxLength) {
+    return <span>{text}</span>;
+  }
+  return (
+    <span>
+      {hidden ? `${text.substr(0, maxLength).trim()} ...`: text}
+      {hidden ? (
+        <a onClick={() => setHidden(false)}> read more</a>
+      ) : (
+        <a onClick={() => setHidden(true)}> read less</a>
+      )}
+    </span>
+  );
+}
+
+export default LessText;
