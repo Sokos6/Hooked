776ddea8cdd2374dc0c206b33ec6fa3adc2348e7
Sokos6 <will.sokolowski@gmail.com>
2020-01-26 00:19:28 -0600

    stepped

--- stateside/src/App.js
+++ stateside/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import LessText from './LessText';
+import Stepped from './Stepped';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       and just keep taking the next step
       towards completing it.`}
     maxLength={35}/>
+    <Stepped />
     </div>
   );
 }
